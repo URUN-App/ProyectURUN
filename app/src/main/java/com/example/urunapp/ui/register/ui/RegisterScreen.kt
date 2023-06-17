b51071b0823efc71e5d2abf47e1581d1aa67c099
Moiezequiel <00038221@uca.edu.sv>
2023-06-17 11:20:10 -0600

    Adding Scroll

--- app/src/main/java/com/example/urunapp/ui/register/ui/RegisterScreen.kt
+++ app/src/main/java/com/example/urunapp/ui/register/ui/RegisterScreen.kt
@@ -12,7 +12,9 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.text.KeyboardOptions
+import androidx.compose.foundation.verticalScroll
 import androidx.compose.material3.Button
 import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.ExperimentalMaterial3Api
@@ -41,8 +43,8 @@ fun   RegisterScreen(navController: NavController) {
     Box(
         Modifier
             .fillMaxSize()
-
             .background(Color(0xFF1E1E1E))
+            .verticalScroll(rememberScrollState())
     ) {
         Register(Modifier.align(Alignment.Center),
             viewModel = RegisterViewModel(), navController)
@@ -73,6 +75,7 @@ fun Register(modifier: Modifier, viewModel: RegisterViewModel, navController: Na
         RegisterButton(navController)
         Spacer(modifier = Modifier.padding(16.dp))
         AccountHave(Modifier.align(Alignment.CenterHorizontally))
+        Spacer(modifier = Modifier.padding(16.dp))
     }
 }
 
