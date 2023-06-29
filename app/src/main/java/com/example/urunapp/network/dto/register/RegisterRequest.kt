5eb5097cce0c7ffdf57207cae494f5d4de9bcf55
Gaenco <00037021@uca.edu.sv>
2023-06-28 19:40:11 -0600

    :card_file_box:Creating RegisterRequest and RegisterResponse for the API

---
+++ app/src/main/java/com/example/urunapp/network/dto/register/RegisterRequest.kt
@@ -0,0 +1,8 @@
+package com.example.urunapp.network.dto.register
+
+data class RegisterRequest(
+    val name: String,
+    val email: String,
+    val password: String,
+    val confirmPassword: String,
+)
