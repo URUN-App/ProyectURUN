b4bd6ed921c4b8956cbd250505856699af794d79
Gaenco <00037021@uca.edu.sv>
2023-07-02 14:54:05 -0600

    :construction:Modifying Navigation to accept an companion object

--- app/src/main/java/com/example/urunapp/navigation/AppNavigation.kt
+++ app/src/main/java/com/example/urunapp/navigation/AppNavigation.kt
@@ -1,34 +1,42 @@
 package com.example.urunapp.navigation
 
 import androidx.compose.runtime.Composable
+import androidx.lifecycle.ViewModelProvider
+import androidx.lifecycle.viewmodel.initializer
+import androidx.lifecycle.viewmodel.viewModelFactory
 import androidx.navigation.NavController
+import androidx.navigation.NavHostController
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
-import androidx.navigation.compose.navigation
 import androidx.navigation.compose.rememberNavController
+import com.example.urunapp.RetrofitApplication
 import com.example.urunapp.SplashScreen
+import com.example.urunapp.repository.CredentialsRepository
 import com.example.urunapp.ui.login.ui.LoginScreen
 import com.example.urunapp.ui.login.ui.LoginViewModel
 import com.example.urunapp.ui.register.ui.RegisterScreen
 import com.example.urunapp.ui.start.ui.StartScreen
 
-
 @Composable
-fun AppNavigation() {
-    val NavController = rememberNavController()
-    NavHost(navController = NavController, startDestination = AppScreens.SplashScreen.route) {
+fun AppNavigation(app: RetrofitApplication, navController: NavHostController) {
+    val viewModelFactory = viewModelFactory {
+        initializer {
+            LoginViewModel(app.credentialsRepository)
+        }
+    }
+
+    NavHost(navController = navController, startDestination = AppScreens.SplashScreen.route) {
         composable(AppScreens.SplashScreen.route) {
-            SplashScreen(NavController)
+            SplashScreen(navController)
         }
         composable(AppScreens.StartScreen.route) {
-            StartScreen(NavController)
+            StartScreen(navController)
         }
-        composable(AppScreens.LoginScreen.route){
-            LoginScreen(viewModel = LoginViewModel())
+        composable(AppScreens.LoginScreen.route) {
+            LoginScreen(viewModel = ViewModelProvider(navController.getViewModelStoreOwner(AppScreens.LoginScreen.route.hashCode()), viewModelFactory).get(LoginViewModel::class.java))
         }
-        composable(AppScreens.RegisterScreen.route){
-            RegisterScreen(NavController)
+        composable(AppScreens.RegisterScreen.route) {
+            RegisterScreen(navController)
         }
-
     }
 }
