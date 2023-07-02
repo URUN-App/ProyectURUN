2d9f6fe0d9077ec5352c0ba9b9f5027a716c1505
Moiezequiel <00038221@uca.edu.sv>
2023-07-02 09:03:13 -0600

    adding icon

--- app/src/main/java/com/example/urunapp/MainActivity.kt
+++ app/src/main/java/com/example/urunapp/MainActivity.kt
@@ -26,8 +26,7 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-
-                   AppNavigation()
+                    AppNavigation()
 
 
 
