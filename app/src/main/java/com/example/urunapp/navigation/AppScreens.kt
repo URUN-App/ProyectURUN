e26049e5c2beee60b654b11201791c75ec1383c8
Gaenco <00037021@uca.edu.sv>
2023-07-07 16:20:51 -0600

    adding the route to WelcomeScreen

--- app/src/main/java/com/example/urunapp/navigation/AppScreens.kt
+++ app/src/main/java/com/example/urunapp/navigation/AppScreens.kt
@@ -1,6 +1,4 @@
 package com.example.urunapp.navigation
-import androidx.navigation.NavController
-import com.example.urunapp.ui.login.ui.LoginScreen
 
 sealed class AppScreens(val route:String){
     object SplashScreen: AppScreens("splash_screen")
