3e3106e492e9ada4bca361d938569b99b29025c6
orlngt23 <00029221@uca.edu.sv>
2023-07-08 21:29:59 -0600

    creando la sealed class de AuthScreen

--- app/src/main/java/com/example/urunapp/graphs/AuthNavGraph.kt
+++ app/src/main/java/com/example/urunapp/graphs/AuthNavGraph.kt
@@ -1,2 +1,20 @@
 package com.example.urunapp.graphs
 
+import androidx.navigation.NavController
+import androidx.navigation.NavGraphBuilder
+import androidx.navigation.NavHostController
+import androidx.navigation.navigation
+
+fun NavGraphBuilder.autNavGraph(navController: NavHostController){
+
+
+}
+
+sealed class AuthScreen(val route: String){
+    object LoginScreen: AuthScreen("LOGIN")
+    object RegisterScreen: AuthScreen("REGISTER")
+    object SplashScreen: AuthScreen("splash_screen")
+    object StartScreen: AuthScreen("start_screen")
+
+}
+
